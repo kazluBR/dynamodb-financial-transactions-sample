22c7f015b9c04b0f16d82593bedc3c502cfeb15c
Lucas Rollemberg <lucas.rollemberg@gmail.com>
2022-11-21 12:00:52 -0300

    Loading data from request parameters

--- app/handlers/load-data/support/bundle.js
+++ app/handlers/load-data/support/bundle.js
@@ -4,79 +4,44 @@ import momentRandom from "moment-random";
 
 export class Bundle {
   constructor(event, context) {
-    this.event = event;
     this.context = context;
-    this.batchSize = 100;
+    if (event.requestContext.http.method != "POST") {
+      throw new Error("Method Not Allowed");
+    }
 
-    this.income_statements = ["Credit Card", "Pix", "Invoice", "Cash"];
-    this.cost_statements = [
-      "Payroll",
-      "Energy",
-      "Water",
-      "Tax",
-      "Transport",
-      "Food",
-      "Reform",
-      "Gifts",
-    ];
+    const payload = JSON.parse(event.body);
+    this.statements = payload.statements;
   }
 
   createStatements = async () => {
-    for (let i = 0; i < this.income_statements.length; i++) {
+    for (let statement of this.statements) {
       let content = {
-        id: i + 1,
-        name: this.income_statements[i],
-        type: "INCOME",
+        id: statement.id,
+        name: statement.name,
+        type: statement.type,
       };
       await Repository.createStatement({ content });
     }
-    console.log(`${this.income_statements.length} income statements created`);
-
-    for (let i = 0; i < this.cost_statements.length; i++) {
-      let content = {
-        id: i + this.income_statements.length + 1,
-        name: this.cost_statements[i],
-        type: "COST",
-      };
-      await Repository.createStatement({ content });
-    }
-    console.log(`${this.cost_statements.length} cost statements created`);
   };
 
   createTransactions = async () => {
-    let total = 0;
-    while (this.context.getRemainingTimeInMillis() > 30000) {
+    for (let statement of this.statements) {
       let promises = [];
-      for (let i = 0; i < this.batchSize; i++) {
-        promises.push(await this.generateIncomeTransactions());
-        promises.push(await this.generateCostTransactions());
+      for (let i = 0; i < statement.count; i++) {
+        promises.push(await this.generateTransaction(statement));
       }
-
       await Promise.all(promises);
-      total += this.batchSize * 2;
-      console.log(`Total transactions created (${total})`);
     }
   };
 
-  generateIncomeTransactions = async () => {
+  generateTransaction = async (statement) => {
     let content = {
       id: uuidv4(),
-      statement: Math.floor(Math.random() * this.cost_statements.length) + 1,
-      value: parseFloat((Math.random() * 5000).toFixed(2)),
-      datetime: momentRandom(Date.now(), new Date("01/01/2020")),
-    };
-    await Repository.createTransaction({ content });
-  };
-
-  generateCostTransactions = async () => {
-    let content = {
-      id: uuidv4(),
-      statement:
-        Math.floor(Math.random() * this.cost_statements.length) +
-        this.income_statements.length +
-        1,
-      value: parseFloat((Math.random() * 3000).toFixed(2)),
-      datetime: momentRandom(Date.now(), new Date("01/01/2020")),
+      statement: statement.id,
+      value: parseFloat(
+        (Math.random() * statement.range.to + statement.range.from).toFixed(2)
+      ),
+      datetime: momentRandom(Date.now(), new Date(process.env.INITIAL_DATE)),
     };
     await Repository.createTransaction({ content });
   };
